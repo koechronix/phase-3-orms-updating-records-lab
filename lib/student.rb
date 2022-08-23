dade3b7a68fd3a18db8f5d1b8175feb0c279f240
koechronix <koechronix@yahoo.com>
2022-08-22 23:02:08 -0400

    commit tree f60ff9c50993fb14a451f0b629f552cd9c24fa7e

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,33 @@
 require_relative "../config/environment.rb"
 
 class Student
+#  attr_accessor :name, :grade, :id 
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+#   def initialize(name:, grade:, id: nil)
+#    @id = id
+#    @name = name
+#    @grade = grade
+#   end
+attr_accessor :name, :grade, :id
+  
+    def initialize(name:, grade:, id: nil)
+      @id = id
+      @name = name
+      @grade = grade
+    end
+  
+  #  create table
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end
 
+  
 
 end
